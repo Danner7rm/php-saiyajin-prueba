243dc61b3da26c01835fb9fe8012f06157454ba4
Danner7rm <rodriguezmosquera05@gmail.com>
2025-10-27 18:05:47 -0500

    clase 17

--- index.php
+++ index.php
@@ -1,19 +1,13 @@
 <?php
 
 
-
-require_once "App/Traits/tecnicasSimples.php";
-require_once "App/Traits/tecnicasEspeciales.php";
-require_once "App/Traits/tecnicasCombinadas.php";
-require_once "App/Clases/Saiyajin.php";
-require_once "App/Clases/Supersaiyajin.php";
-require_once "App/Otras/Saiyajin.php";
+require_once "autoload.php";
 
 use App\Clases\Saiyajin;
-use App\Clases\SUperSaiyajin;
-use App\Otras\Saiyajin as OtroSaiyajin;
+use App\Clases\SuperSaiyajin;
 
-$goku = new OtroSaiyajin(nivel_pelea: 1000, nombre: "goku");
+
+$goku = new Saiyajin(nivel_pelea: 1000, nombre: "goku");
 echo $goku->Saludar();
 
 echo "<br><br>";
@@ -21,14 +15,8 @@ echo "<br><br>";
 $goten = new Saiyajin(nivel_pelea: 950, nombre: "goten");
 echo $goten->Saludar();
 
-echo "<br><br>";
-
-$vegeta = new Saiyajin(nivel_pelea: 950, nombre: "Vegeta");
-echo $vegeta->MostrarColorCabello();
-
-echo "<br><br>";
 
 
 
-$gohan = new App\Clases\Saiyajin(nivel_pelea: 1500, nombre: "gohan");
-echo $gohan->UsarKamehameha();
+
+
